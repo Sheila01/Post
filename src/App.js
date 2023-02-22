4249c27c35359ebd5aef41c22bd598d9a2433998
Sheila Colorado <sheilacolorado1997@gmail.com>
2023-02-22 16:36:21 +0100

    I don't understand

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Search from './Components/Search'
 
 function App() {
   return(
-    <div >
-      <AppTwo/>
-      <Search/>
+    <div className="app container">
+      <div className="jumbotron">
+        <p className="lead text-center">This is search1</p>
+       
+        <Search/>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -6,6 +6,9 @@ const Search = () => {
           <div className='form-group col-md-8'>
             <input type='text' className='form-control form-control-lg' placeholder='Search img'/>
           </div>
+          <div className='form-group col-md-4'>
+            <input type='submit' className='btn btn-lg btn-danger' placeholder='Search img'/>
+          </div>
         </div>
       </form>
     </div>
