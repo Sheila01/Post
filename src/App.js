83afc30642f8b48c968e67554b0bd9e92da9e1dd
Sheila Colorado <sheilacolorado1997@gmail.com>
2023-02-22 15:55:27 +0100

    non-functioning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
+import AppDos from './Components/AppDos';
 
 function App() {
   return(
-    <div className="app container">
-      <div className="jumbotron">
-        <p className="lead text-center">This is search</p>
-      </div>
+    <div >
+      <AppDos/>
     </div>
   );
 }
--- src/Components/AppDos.js
+++ src/Components/AppDos.js
@@ -11,13 +11,17 @@
 export default Component
 */
 
-function Component(){
-  return(
-    <div className="app container">
-      <div className="jumbotron">
-        <p className="lead text-center">This is search</p>
+import React, {Component} from 'react';
+
+class AppDos extends Component{
+  render(){
+    return(
+      <div className='app container'>
+        <div className='jumbotron'>
+          <p className='lead text-center'>This is search</p>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
-export default Component
+export default AppDos
