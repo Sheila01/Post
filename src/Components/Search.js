4dff83b5e5cf6783ffa8e3bf5b554806ca2ddab7
Sheila Colorado <sheilacolorado1997@gmail.com>
2023-02-24 10:36:08 +0100

    I don't understand why it tells me that state is not defined.

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,7 +1,11 @@
-import React from 'react';
-const Search = () => {
+import React, { Component } from 'react';
+/*const Search = () => {
+
   const busquedaRef = React.createRef()
   const obtenerDatos = (e) => {
+    state = {
+      finish:''
+    }
     e.preventeDefault()
     const finish = this.busquedaRef.current.value
     this.props.obtenerDatos(finish)
@@ -21,4 +25,34 @@ const Search = () => {
     </div>
   );
 };
+export default Search;*/
+
+class Search extends Component {
+  busquedaRef = React.createRef();
+  obtenerDatos = (e) => {
+    e.preventeDefault()
+    state = {
+      finish: '',
+    };
+    e.preventeDefault();
+    const finish = this.busquedaRef.current.value;
+    this.props.obtenerDatos(finish);
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.obtenerDatos}>
+          <div className='row'>
+            <div className='form-group col-md-8'>
+              <input ref={this.busquedaRef} type='text' className='form-control form-control-lg' placeholder='Search img' />
+            </div>
+            <div className='form-group col-md-4'>
+              <input type='submit' className='btn btn-lg btn-danger' placeholder='Search img' />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 export default Search;
