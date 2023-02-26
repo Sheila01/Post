a497993292b629f022dacbc2f23c6343822acff3
Sheila Colorado <sheilacolorado1997@gmail.com>
2023-02-26 17:26:31 +0100

    new component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AppTwo from './Components/AppTwo';
+import Result from './Components/Result';
 
 function App() {
   return(
     <div>
       <AppTwo/>
+      <Result/>
     </div>
   )
 }
---
+++ src/Components/Result.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Result = () => {
+  return(
+    <div>
+      <p> This is the new Component</p>
+    </div>
+  )
+}
+export default Result
