a92805b9272ddf96efd3a62b44054623e5ea9d28
Sheila Colorado <sheilacolorado1997@gmail.com>
2023-03-02 12:51:28 +0100

    creating the functionality to start forward

--- src/Components/WebSite.js
+++ src/Components/WebSite.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const WebSite = () => {
+const WebSite = (props
+  ) => {
   return (
     <div className='py-3'>
-      <button type='button' className='btn btn-info mr-1'>
+      <button onClick={props.paginaAnterior} type='button' className='btn btn-info mr-1'>
         Previous &larr;
       </button>
-      <button type='button' className='btn btn-info'>
+      <button onClick={props.paginaSiguiente} type='button' className='btn btn-info'>
         Next &rarr;
       </button>
     </div>
